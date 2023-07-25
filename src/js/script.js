22363fad3ee4cf12c64048dac714481b11a62d90
Kateryna <katepysova97@gmail.com>
2023-07-25 15:56:16 +0200

    Created form component with basic validation

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,21 @@
+import checkEmail from "./form.js";
+
 const menuButtonElement = document.querySelector(".menu");
 const headerElement = document.querySelector(".header");
+const form = document.querySelector(".form");
+const emailElem = document.querySelector("#email");
 
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const isEmailValid = checkEmail(emailElem);
+
+  if (isEmailValid) {
+    /*
+      send data
+    */
+    emailElem.value = "";
+  }
+});
 menuButtonElement.addEventListener("click", () => {
   headerElement.classList.toggle("header--collapsed");
 });
